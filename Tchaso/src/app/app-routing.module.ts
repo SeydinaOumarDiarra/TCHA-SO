79620604a6bd073129490ac71ab1ba39c12b8e0a
SeydinaOumarDiarra <seydinaoumardiarra@gmail.com>
2022-02-01 14:41:50 +0000

    version01022022

--- Tchaso/src/app/app-routing.module.ts
+++ Tchaso/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './Accueil/accueil/accueil.component';
+import { LoginComponent } from './login/login.component';
+import { ServiceComponent } from './Services/service/service.component';
+import { SpecialiteComponent } from './Specialites/specialite/specialite.component';
+import { UtilisateurComponent } from './Utilisateurs/utilisateur/utilisateur.component';
+import { VilleComponent } from './Villes/ville/ville.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'services', component: ServiceComponent },
+  { path: 'specialites', component: SpecialiteComponent },
+  { path: 'villes', component: VilleComponent },
+  { path: 'utilisateurs', component: UtilisateurComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Tchaso/src/app/app.module.ts
+++ Tchaso/src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AccueilComponent } from './Accueil/accueil/accueil.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { ServiceComponent } from './Services/service/service.component';
+import { SpecialiteComponent } from './Specialites/specialite/specialite.component';
+import { UtilisateurComponent } from './Utilisateurs/utilisateur/utilisateur.component';
+import { VilleComponent } from './Villes/ville/ville.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AccueilComponent,
+    HeaderComponent,
+    MenuComponent,
+    ServiceComponent,
+    SpecialiteComponent,
+    UtilisateurComponent,
+    VilleComponent
   ],
   imports: [
     BrowserModule,
+    Ng2SearchPipeModule,
     AppRoutingModule
   ],
   providers: [],
